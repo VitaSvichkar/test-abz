d773eed22c4a033e6a1b2da5c9deed66c39cb338
Viktoriia Svichkar <vitasvichkar@gmail.com>
2025-08-27 23:10:22 +0300

    fix: floating label no longer overlaps input text

--- project/src/Components/Form/Input/Input.jsx
+++ project/src/Components/Form/Input/Input.jsx
@@ -3,7 +3,7 @@ import c from './_input.module.scss';
 export const Input = ({ text, type, helperText }) => {
   return (
     <label className={c.label}>
-      <input className={c.input} type={type} />
+      <input className={c.input} type={type} placeholder=" " />
       <span className={c.labelText}>{text}</span>
 
       {helperText && <span className={c.helperText}>{helperText}</span>}
