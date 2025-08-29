23388fb0eadbbbf881ad1d92b9e4dce6def043fa
Viktoriia Svichkar <vitasvichkar@gmail.com>
2025-08-29 22:03:07 +0300

    feat: add validation for name, email and phone input fields

--- project/src/Components/Form/FormBlock.jsx
+++ project/src/Components/Form/FormBlock.jsx
@@ -4,23 +4,87 @@ import { Position } from './Positions/Position';
 import { getPositions } from '../../api/positions';
 import { Input } from './Input/Input';
 import { UploadFile } from './UploadFile/UploadFile';
+import { useForm } from 'react-hook-form';
 
 export const FormBlock = () => {
+  const regPhone = /^38\d{10}$/;
   const [positions, setPositions] = useState([]);
+
+  const {
+    register,
+    formState: { errors, isValid, touchedFields },
+    watch,
+    handleSubmit,
+    reset,
+  } = useForm({ mode: 'all' });
+
+  const nameVal = watch('name') ?? '';
+  const emailVal = watch('email') ?? '';
+  const phoneVal = watch('phone') ?? '';
+
+  const isNameFilled = !!nameVal.trim();
+  const isEmailFilled = !!emailVal.trim();
+  const isPhoneFilled = !!phoneVal.trim();
 
   useEffect(() => {
     getPositions().then((data) => setPositions(data.positions));
   }, []);
 
+  const onSubmit = () => {
+    reset();
+  };
+
   return (
     <section className={c.formBlock} id="signUp">
       <h2>Working with POST request</h2>
 
-      <form className={c.form}>
+      <form className={c.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={c.fields}>
-          <Input text="Your name" type="text" />
-          <Input text="Email" type="email" />
-          <Input text="Phone" type="tel" helperText="+38 (XXX) XXX - XX - XX" />
+          <Input
+            text="Your name"
+            type="text"
+            isFilled={isNameFilled}
+            touched={touchedFields?.name}
+            errors={errors?.name}
+            {...register('name', {
+              required: 'This field is required',
+              minLength: { value: 2, message: 'At least 2 characters' },
+              maxLength: { value: 30, message: 'Maximum 30 characters' },
+            })}
+          />
+
+          <Input
+            text="Email"
+            type="email"
+            isFilled={isEmailFilled}
+            touched={touchedFields?.email}
+            errors={errors?.email}
+            {...register('email', {
+              required: 'This field is required',
+              maxLength: { value: 50, message: 'Maximum 50 characters' },
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: 'Invalid email address',
+              },
+            })}
+          />
+          <Input
+            text="Phone"
+            type="tel"
+            helperText="+38 (XXX) XXX - XX - XX"
+            isFilled={isPhoneFilled}
+            touched={touchedFields?.phone}
+            errors={errors?.phone}
+            {...register('phone', {
+              required: 'This field is required',
+              maxLength: { value: 12, message: 'Maximum 12 digits' },
+              pattern: {
+                value: regPhone,
+                message:
+                  'Invalid phone number. Use format: 38 (XXX) XXX - XX - XX',
+              },
+            })}
+          />
         </div>
 
         {/* Positions */}
@@ -37,7 +101,7 @@ export const FormBlock = () => {
         <UploadFile />
 
         <div className={c.wrapBtn}>
-          <button className="signUpBtn" disabled>
+          <button className="signUpBtn" disabled={!isValid}>
             Sign up
           </button>
         </div>
--- project/src/Components/Form/Input/Input.jsx
+++ project/src/Components/Form/Input/Input.jsx
@@ -1,12 +1,26 @@
 import c from './_input.module.scss';
+import { forwardRef } from 'react';
 
-export const Input = ({ text, type, helperText }) => {
-  return (
-    <label className={c.label}>
-      <input className={c.input} type={type} placeholder=" " />
-      <span className={c.labelText}>{text}</span>
+export const Input = forwardRef(
+  ({ text, type, helperText, touched, isFilled, errors, ...props }, ref) => {
+    const showText =
+      (touched && isFilled && !errors) || !touched
+        ? helperText
+        : errors?.message;
 
-      {helperText && <span className={c.helperText}>{helperText}</span>}
-    </label>
-  );
-};
+    return (
+      <label className={`${errors ? `${c.error} ${c.label}` : c.label}`}>
+        <input
+          className={c.input}
+          type={type}
+          placeholder=" "
+          ref={ref}
+          {...props}
+        />
+
+        <span className={c.labelText}>{text}</span>
+        {showText && <span className={c.helperText}>{showText}</span>}
+      </label>
+    );
+  }
+);
